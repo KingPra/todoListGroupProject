7c83c1e00a40d8a46bbd7bc8286d4829bc31fe35
King Pra <kingi.pra@gmail.com>
2019-06-28 17:04:09 -0500

    initial commit

---
+++ src/main/java/com/kpr/TodoListGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListGroupProjectApplication.class, args);
+	}
+
+}
